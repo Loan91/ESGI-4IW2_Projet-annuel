14d81239dcc920ab176fa317d3cf93becad485b9
abdoul-rb <abdoulrahim.tools@gmail.com>
2020-12-22 00:13:29 +0100

    [ADD|UPDATE] Registration user with Form and DataFixtures

--- app/src/Repository/UserRepository.php
+++ app/src/Repository/UserRepository.php
@@ -4,6 +4,8 @@ namespace App\Repository;
 
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
@@ -22,9 +24,13 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
         parent::__construct($registry, User::class);
     }
 
-    /**
-     * Used to upgrade (rehash) the user's password automatically over time.
-     */
+  /**
+   * Used to upgrade (rehash) the user's password automatically over time.
+   * @param UserInterface $user
+   * @param string $newEncodedPassword
+   * @throws ORMException
+   * @throws OptimisticLockException
+   */
     public function upgradePassword(UserInterface $user, string $newEncodedPassword): void
     {
         if (!$user instanceof User) {
