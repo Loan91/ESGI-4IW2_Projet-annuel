5ba73aae2c0dbc6ee5940caa5149a7d28d4ff6a5
Fendly <f.belhomme.jouvet@gmail.com>
2021-02-01 17:20:36 +0100

    add forgot password with mail

--- app/src/Entity/User.php
+++ app/src/Entity/User.php
@@ -3,16 +3,13 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
-use Gedmo\Mapping\Annotation as Gedmo;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
- * @ORM\Table(name="`user_account`", schema="immo")
+ * @ORM\Table(name="user_account", schema="immo")
  */
 class User implements UserInterface
 {
@@ -50,83 +47,9 @@ class User implements UserInterface
     private $name;
 
     /**
-     * @ORM\OneToMany(targetEntity=Property::class, mappedBy="owner", orphanRemoval=true)
-     */
-    private $properties;
-
-    /**
-     * @ORM\OneToMany(targetEntity=Contact::class, mappedBy="prospect", orphanRemoval=true)
-     */
-    private $contacts;
-
-    /**
-     * @ORM\OneToMany(targetEntity=Favorite::class, mappedBy="prospect", orphanRemoval=true)
-     */
-    private $favorites;
-
-    /**
      * @ORM\Column(type="string", length=255, nullable=true)
      */
-    private $denomination;
-
-    /**
-     * @ORM\Column(type="string", length=255, nullable=true)
-     */
-    private $firstname;
-
-    /**
-     * @ORM\Column(type="string", length=255, nullable=true)
-     */
-    private $lastname;
-
-    /**
-     * @ORM\Column(type="string", length=255, nullable=true)
-     */
-    private $gender;
-
-    /**
-     * @ORM\Column(type="date", nullable=true)
-     */
-    private $birthday;
-
-    /**
-     * @ORM\Column(type="string", length=255, nullable=true)
-     */
-    private $address;
-
-    /**
-     * @ORM\Column(type="integer", nullable=true)
-     */
-    private $zipCode;
-
-    /**
-     * @ORM\Column(type="string", length=255, nullable=true)
-     */
-    private $city;
-
-    /**
-     * @ORM\Column(type="string", length=255, nullable=true)
-     */
-    private $country;
-
-    /**
-     * @Gedmo\Timestampable("create")
-     * @ORM\Column(type="datetime", nullable=true)
-     */
-    private $createdAt;
-
-    /**
-     * @Gedmo\Timestampable("update")
-     * @ORM\Column(type="datetime", nullable=true)
-     */
-    private $updatedAt;
-
-    public function __construct()
-    {
-        $this->properties = new ArrayCollection();
-        $this->contacts = new ArrayCollection();
-        $this->favorites = new ArrayCollection();
-    }
+    private $reset_token;
 
     public function getId(): ?int
     {
@@ -218,224 +141,14 @@ class User implements UserInterface
         return $this;
     }
 
-    /**
-     * @return Collection|Property[]
-     */
-    public function getProperties(): Collection
+    public function getResetToken(): ?string
     {
-        return $this->properties;
+        return $this->reset_token;
     }
 
-    public function addProperty(Property $property): self
+    public function setResetToken(?string $reset_token): self
     {
-        if (!$this->properties->contains($property)) {
-            $this->properties[] = $property;
-            $property->setOwner($this);
-        }
-
-        return $this;
-    }
-
-    public function removeProperty(Property $property): self
-    {
-        if ($this->properties->removeElement($property)) {
-            // set the owning side to null (unless already changed)
-            if ($property->getOwner() === $this) {
-                $property->setOwner(null);
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|Contact[]
-     */
-    public function getContacts(): Collection
-    {
-        return $this->contacts;
-    }
-
-    public function addContact(Contact $contact): self
-    {
-        if (!$this->contacts->contains($contact)) {
-            $this->contacts[] = $contact;
-            $contact->setProspect($this);
-        }
-
-        return $this;
-    }
-
-    public function removeContact(Contact $contact): self
-    {
-        if ($this->contacts->removeElement($contact)) {
-            // set the owning side to null (unless already changed)
-            if ($contact->getProspect() === $this) {
-                $contact->setProspect(null);
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|Favorite[]
-     */
-    public function getFavorites(): Collection
-    {
-        return $this->favorites;
-    }
-
-    public function addFavorite(Favorite $favorite): self
-    {
-        if (!$this->favorites->contains($favorite)) {
-            $this->favorites[] = $favorite;
-            $favorite->setProspect($this);
-        }
-
-        return $this;
-    }
-
-    public function removeFavorite(Favorite $favorite): self
-    {
-        if ($this->favorites->removeElement($favorite)) {
-            // set the owning side to null (unless already changed)
-            if ($favorite->getProspect() === $this) {
-                $favorite->setProspect(null);
-            }
-        }
-
-        return $this;
-    }
-
-    public function getDenomination(): ?string
-    {
-        return $this->denomination;
-    }
-
-    public function setDenomination(?string $denomination): self
-    {
-        $this->denomination = $denomination;
-
-        return $this;
-    }
-
-    public function getFirstname(): ?string
-    {
-        return $this->firstname;
-    }
-
-    public function setFirstname(string $firstname): self
-    {
-        $this->firstname = $firstname;
-
-        return $this;
-    }
-
-    public function getLastname(): ?string
-    {
-        return $this->lastname;
-    }
-
-    public function setLastname(string $lastname): self
-    {
-        $this->lastname = $lastname;
-
-        return $this;
-    }
-
-    public function getGender(): ?string
-    {
-        return $this->gender;
-    }
-
-    public function setGender(string $gender): self
-    {
-        $this->gender = $gender;
-
-        return $this;
-    }
-
-    public function getBirthday(): ?\DateTimeInterface
-    {
-        return $this->birthday;
-    }
-
-    public function setBirthday(\DateTimeInterface $birthday): self
-    {
-        $this->birthday = $birthday;
-
-        return $this;
-    }
-
-    public function getAddress(): ?string
-    {
-        return $this->address;
-    }
-
-    public function setAddress(string $address): self
-    {
-        $this->address = $address;
-
-        return $this;
-    }
-
-    public function getZipCode(): ?int
-    {
-        return $this->zipCode;
-    }
-
-    public function setZipCode(int $zipCode): self
-    {
-        $this->zipCode = $zipCode;
-
-        return $this;
-    }
-
-    public function getCity(): ?string
-    {
-        return $this->city;
-    }
-
-    public function setCity(string $city): self
-    {
-        $this->city = $city;
-
-        return $this;
-    }
-
-    public function getCountry(): ?string
-    {
-        return $this->country;
-    }
-
-    public function setCountry(string $country): self
-    {
-        $this->country = $country;
-
-        return $this;
-    }
-
-    public function getCreatedAt(): ?\DateTimeInterface
-    {
-        return $this->createdAt;
-    }
-
-    public function setCreatedAt(\DateTimeInterface $createdAt): self
-    {
-        $this->createdAt = $createdAt;
-
-        return $this;
-    }
-
-    public function getUpdatedAt(): ?\DateTimeInterface
-    {
-        return $this->updatedAt;
-    }
-
-    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
-    {
-        $this->updatedAt = $updatedAt;
+        $this->reset_token = $reset_token;
 
         return $this;
     }
