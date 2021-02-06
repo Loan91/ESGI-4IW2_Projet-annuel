7c900732157dbde134bd637fecb378e38857a769
Fendly <f.belhomme.jouvet@gmail.com>
2021-02-06 15:15:51 +0100

    update page profile and add edit profile

--- app/src/Controller/Security/ProfileUserController.php
+++ app/src/Controller/Security/ProfileUserController.php
@@ -3,6 +3,8 @@
 namespace App\Controller\Security;
 
 
+use App\Form\EditProfileType;
+use http\Env\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,5 +17,30 @@ class ProfileUserController extends AbstractController
     public function index()
     {
         return $this->render('users/index.html.twig');
+    }
+
+    /**
+     * @Route("/users/profile/modifier", name="app_user_profil_modifier")
+     * @param Request $request
+     */
+    public function editProfile(Request $request)
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(EditProfileType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash('success', 'Profil mis à jour');
+            return $this->redirectToRoute('app_users');
+        }
+
+        return $this->render('users/editprofile.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
