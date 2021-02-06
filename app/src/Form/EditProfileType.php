f74f37ebfc231917dab3c9ec8cc90e45d8c579b8
Fendly <f.belhomme.jouvet@gmail.com>
2021-02-06 15:38:42 +0100

    update page profile and add edit profile

--- app/src/Form/EditProfileType.php
+++ app/src/Form/EditProfileType.php
@@ -19,7 +19,10 @@ class EditProfileType extends AbstractType
         $builder
             ->add('name', TextType::class)
             ->add('email', EmailType::class)
-            ->add('Valider', SubmitType::class);
+            ->add([
+                'Valider', SubmitType::class,
+                'class' => 'py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
