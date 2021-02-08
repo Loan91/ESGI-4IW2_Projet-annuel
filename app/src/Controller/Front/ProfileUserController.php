88f674e0c295a74e6b3b0201a23a73d5599725e5
Fendly <f.belhomme.jouvet@gmail.com>
2021-02-08 16:18:14 +0100

    add delete in profile user

--- app/src/Controller/Front/ProfileUserController.php
+++ app/src/Controller/Front/ProfileUserController.php
@@ -7,6 +7,7 @@ use App\Entity\User;
 use App\Form\EditProfileType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
@@ -84,10 +85,8 @@ class ProfileUserController extends AbstractController
      */
     public function delete(Request $request, User $user): Response
     {
-        if($user == null)
-        {
-            return $this->redirect($this->generateUrl('default_index'));
-        }
+        $session = new Session();
+        $session->invalidate();
 
         if ($this->isCsrfTokenValid('delete' . $user->getId(), $request->request->get('_token'))) {
             $entityManager = $this->getDoctrine()->getManager();
