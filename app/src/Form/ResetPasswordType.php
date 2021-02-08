a8c78848bb474b68f5aaa8bdc26715c227ff9138
Fendly <f.belhomme.jouvet@gmail.com>
2021-02-08 17:18:56 +0100

    update

--- app/src/Form/ResetPasswordType.php
+++ app/src/Form/ResetPasswordType.php
@@ -14,39 +14,16 @@ class ResetPasswordType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('password', RepeatedType::class, [
-                'type'            => PasswordType::class,
-                'invalid_message' => "Les mot de passe doivent être identiques.",
-                'required'        => true,
-                'first_options'   => [
-                    'label'       => 'Mot de passe',
-                    'label_attr'  => [
-                        'title'   => 'Pour des raisons de sécurité, votre mot de passe doit contenir au minimum 8 caractères au moins'
-                    ],
-                    'attr'          => [
-                        'pattern'   => "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
-                        'title'     => "Pour des raisons de sécurité, votre mot de passe doit contenir au minimum 8 caractères au moins, au moins une lettre majuscule, une lettre minuscule, un chiffre et un caractère spécial",
-                        'maxlength' => 255
-                    ]
-                ],
-                'second_options'  => [
-                    'label'       => 'Confirmez le mot de passe',
-                    'label_attr'  => [
-                        'title'   => 'Confirmez votre mot de passe.'
-                    ],
-                    'attr'          => [
-                        'pattern'   => "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
-                        'title'     => "Confirmez votre mot de passe",
-                        'maxlength' => 255
-                    ]
-                ]
-            ]);
+            ->add('email', EmailType::class)
+            ->add('envoyer', SubmitType::class)
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => User::class,
+            // Configure your form options here
         ]);
+
     }
 }
