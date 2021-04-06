a9e88d34d0bf379325cc64d3ae3f61aae05db1a8
Nolikein <wasik.come@gmail.com>
2021-04-06 22:15:06 +0200

    Update forgot password form with tailwind theme

--- app/src/Form/ResetPasswordType.php
+++ app/src/Form/ResetPasswordType.php
@@ -7,14 +7,26 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ResetPasswordType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email', EmailType::class)
-            ->add('envoyer', SubmitType::class)
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse Email',
+                'empty_data' => '',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Le champ email ne peut être vide'
+                    ]),
+                    new Email([
+                        'message' => 'L\'email "{{ value }}" n\'est pas valide.',
+                    ])
+                ]
+            ])
         ;
     }
 
