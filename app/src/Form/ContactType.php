c70ea2369c578f3a67911310b5deedae301c824c
Loan Besson <loan.besson@hotmail.fr>
2021-06-05 21:46:46 +0200

    Create contact

---
+++ app/src/Form/ContactType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contact;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('desiredDate', null, [
+                'label' => 'Date désirée',
+                'widget' => 'single_text',
+                'html5' => 'true',
+                'empty_data' => ''
+            ])
+            ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Contact::class,
+        ]);
+    }
+}
