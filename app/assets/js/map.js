eed5154872d87f85c7ee9dccdaa56aaad60aea2e
Dorian Gueguen <doriangueguen@orange.fr>
2021-07-01 18:13:02 +0200

    end map

--- app/assets/js/map.js
+++ app/assets/js/map.js
@@ -1,1 +1,35 @@
-console.log('ca fonctionne ?')
+var lng;
+var lat;
+
+var adress = document.querySelector('#adressToMap').textContent;
+var zipCode = document.querySelector('#zipCodeToMap').textContent;
+var city = document.querySelector('#cityToMap').textContent;
+var formatedAdress = adress.replace(' ', '%20');
+var theUrl = "https://api-adresse.data.gouv.fr/search/?q="+formatedAdress+"%20"+zipCode+"%20"+city+"&type=street&autocomplete=0"
+fetch(theUrl)
+    .then(resp => resp.json())
+    .then( data => {
+        lng = data.features[0].geometry.coordinates[0];
+        lat = data.features[0].geometry.coordinates[1];
+        myMap(lng, lat)
+
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
+function myMap(lng, lat){
+    var mymap = L.map('mapid').setView([lat, lng], 17);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mymap);
+    var circle = L.circle([lat, lng], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 50
+    }).addTo(mymap);
+
+}
+
+
