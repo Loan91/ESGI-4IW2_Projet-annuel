d313ecd086738c448b2574dd660189f38c6ab585
Fendly98 <f.belhomme.jouvet@gmail.com>
2021-07-13 14:14:19 +0200

    update code

--- app/src/Form/ContactEmailType.php
+++ app/src/Form/ContactEmailType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
