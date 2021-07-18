12566d4810bf9656ba91595b1a3ecd4848c26954
Fendly98 <f.belhomme.jouvet@gmail.com>
2021-07-18 10:41:36 +0200

    fixed

--- app/src/Security/FacebookAuthenticator.php
+++ app/src/Security/FacebookAuthenticator.php
@@ -130,10 +130,7 @@ class FacebookAuthenticator extends SocialAuthenticator
      */
     public function start(Request $request, AuthenticationException $authException = null)
     {
-        return new RedirectResponse(
-            '/connect/', // might be the site, where users choose their oauth provider
-            Response::HTTP_TEMPORARY_REDIRECT
-        );
+        return new RedirectResponse('/login');
     }
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
